e40b7cdaeed1a7e5b21be8b36c598a0a3e43aacc
egurinko <egctoru@gmail.com>
2019-08-05 02:24:21 +0900

    Added vuetify config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
+import "babel-polyfill";
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+
+Vue.config.productionTip = false;
 
 new Vue({
   el: "#app",
   store,
+  vuetify,
   render: h => h(App)
 });
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,13 @@
+import "@mdi/font/css/materialdesignicons.css";
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import theme from "./theme";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme,
+  icons: {
+    iconfont: "mdi"
+  }
+});
