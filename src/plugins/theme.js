89da28add71a9b91df9008bde966552622c82fef
egurinko <egctoru@gmail.com>
2019-08-05 02:24:08 +0900

    Added theme

---
+++ src/plugins/theme.js
@@ -0,0 +1,31 @@
+export default {
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: "#33333d",
+        secondary: "#373740",
+        tertiary: "#27272f",
+        deepGreen: "#005c57",
+        middleGreen: "#08b97f",
+        lightGreen: "#38efba",
+        lightWarning: "#ffdc77",
+        deepWarning: "#ffd7d0",
+        lightCritical: "#ff6851",
+        deepCritical: "#ff847c"
+      },
+      light: {
+        primary: "#33333d",
+        secondary: "#373740",
+        tertiary: "#27272f",
+        deepGreen: "#005c57",
+        middleGreen: "#08b97f",
+        lightGreen: "#38efba",
+        lightWarning: "#ffdc77",
+        deepWarning: "#ffd7d0",
+        lightCritical: "#ff6851",
+        deepCritical: "#ff847c"
+      }
+    }
+  }
+};
