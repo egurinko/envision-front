d7970d9482d92315023b37cac3a7756e2415f369
egurinko <egctoru@gmail.com>
2019-08-09 01:58:14 +0900

    callAPI

---
+++ src/components/common/Response.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="primary pb-3">
+    <v-alert
+      :value="response.status === 200 && response.method !== 'GET'"
+      color="lightGreen"
+      icon="check_circle"
+      outlined
+      tile
+    >
+      Succeeded !
+    </v-alert>
+
+    <v-alert
+      :value="response.status !== '' && response.status !== 200"
+      color="lightCritical"
+      icon="warning"
+      outlined
+      tile
+    >
+      {{ response.errors.errorMessage }}
+    </v-alert>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "Response",
+  computed: {
+    ...mapState({
+      response: state => state.response
+    })
+  },
+  created() {},
+  methods: {}
+};
+</script>
+
+<style></style>
---
+++ src/modules/callAPI.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+import { getCookie } from "./controllCookie";
+import store from "../store";
+
+// This function is for making the interceptor to check and set the token in cookie
+
+// INSIDE OF REQUEST
+//  METHOD: GET/POST/PUT/DELETE
+//  DOMAIN: localhost...
+//  DATA: for post request
+//  PARAMS: for get request
+
+export default requests => {
+  const reqPromises = [];
+  store.commit("updateIsLoading", true);
+  for (const request of requests) {
+    reqPromises.push(requestAPI(request));
+  }
+
+  return Promise.all(reqPromises)
+    .then(responses => {
+      store.commit("setResponse", {
+        status: 200,
+        method: requests[0].method,
+        errors: ""
+      });
+      return responses.map(response => response.data);
+    })
+    .catch(error => {
+      if (error.response) {
+        store.commit("setResponse", {
+          status: error.response.status,
+          method: requests[0].method,
+          errors: error.response.data
+        });
+      } else {
+        store.commit("setResponse", {
+          status: 500,
+          method: requests[0].method,
+          errors: { errorMessage: "500 Sorry it's me, not you." }
+        });
+      }
+      console.log("CATCH CALL API: ", error.response);
+    })
+    .finally(error => {
+      store.commit("updateIsLoading", false);
+    });
+};
+
+const requestAPI = request => {
+  const config = { baseURL: request.url, method: request.method };
+
+  if (request.method === "GET") {
+    if (request.params) {
+      config.params = request.params;
+    }
+  } else {
+    const { token } = getCookie();
+    config.headers = { "x-access-token": token };
+
+    if (request.method === "POST") {
+      config.data = request.data;
+    }
+  }
+
+  return axios.request(config);
+};
