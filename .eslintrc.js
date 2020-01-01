5dfaa9126e07ce4658bfb5149b0c846ec4295e40
egurinko <egctoru@gmail.com>
2020-01-01 19:25:50 +0900

    added eslint to cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,16 +7,22 @@ module.exports = {
     "plugin:vue/recommended",
     "eslint:recommended",
     "prettier/vue",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
+    "plugin:cypress/recommended"
   ],
   rules: {
     "vue/component-name-in-template-casing": ["error", "PascalCase"],
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "cypress/no-assigning-return-values": "error",
+    "cypress/no-unnecessary-waiting": "error",
+    "cypress/assertion-before-screenshot": "warn"
   },
   globals: {
-    $nuxt: true
+    $nuxt: true,
+    "cypress/globals": true
   },
+  plugins: ["cypress"],
   parserOptions: {
     parser: "babel-eslint"
   }
